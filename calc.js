b6ec00fb4cda8b58c2c8e9dd2cf35a13d76b36c1
Yexin Wang <wang.yexin@outlook.com>
2019-09-14 17:28:30 -0400

    Use function as constructors to solve the situation which calculator and calculator states should hold each other's reference.

--- calc.js
+++ calc.js
@@ -1,29 +1,95 @@
-/* TODO: make sure the web page render: window.addEventListener("load", init, false) */
-
-'use strict';
-
 (function () {
-    const calculator = {
-        display: "0",
-        firstOperand: null,
-        operator: null,
-        secondOperand: null,
-        currentState: null,
+    'use strict';
 
-        /*TODO: add key listener here, distinguish input type and call something like currentState.handleDigit() or
-           currentState.handleClear(), let the currentState to handle the input and state transition */
+    /* define a set of states, all of them should have same functions to implement polymorphism */
+    function CalcFirstOperandState(calc) {
+        this.calc = calc;
+        this.handleOperand = function (digit) {
+            this.calc.firstOperand += digit;
+            this.calc.display = this.calc.firstOperand;
+        };
+
+        this.handleDot = function (dot) {
+            this.calc.firstOperand += dot;
+            this.calc.display = this.calc.firstOperand;
+            this.calc.currentState = this.calc.calcFirstOperandDecimalState;
+        }
+
+    }
 
 
+    function CalcFirstOperandDecimalState(calc) {
+    }
+
+    function CalcOperatorState(calc) {
+    }
+
+    function CalcSecondOperandState(calc) {
+    }
+
+    function CalcSecondOperandDecimalState(calc) {
+    }
+
+    function Calculator() {
+        this.calcFirstOperandState = new CalcFirstOperandState(this);
+        this.calcFirstOperandDecimalState = new CalcFirstOperandDecimalState(this);
+        this.calcOperatorState = new CalcOperatorState(this);
+        this.calcSecondOperandState = new CalcSecondOperandState(this);
+        this.calcSecondOperandDecimalState = new CalcSecondOperandDecimalState(this);
+
+        this.display = "0";
+        this.firstOperand = "";
+        this.operator = null;
+        this.secondOperand = "";
+        this.currentState = this.calcFirstOperandState;
 
         /*TODO: Maybe this function is not needed, just put the line at end of each handle input*/
-        updateDisplay: function () {
-            document.getElementsByClassName("calculator-screen").value = this.display;
+        this.updateDisplay = function () {
+            const s = document.getElementsByClassName("calculator-screen").item(0)
+            s.value = this.display;
+        };
+
+        this.clear = function () {
+            this.display = "0";
+            this.firstOperand = "";
+            this.operator = null;
+            this.secondOperand = "";
+            this.currentState = this.calcFirstOperandState;
+        };
+
+        this.getInput = function () {
+            /* save this reference */
+            const that = this;
+
+            /* This is an HTML collection objects*/
+            let digitButtons = document.getElementsByClassName("digit");
+            /* use Array.from() convert HTML Collections to array to use forEach */
+            /* For digit buttons*/
+            Array.from(digitButtons).forEach(db => {
+                db.addEventListener("click", event => {
+                    that.currentState.handleOperand(db.value);
+                    that.updateDisplay();
+                })
+            })
+
+            /* For dot */
+            let dots = document.getElementsByClassName("dot");
+            Array.from(dots).forEach(dot => {
+                dot.addEventListener("click", event => {
+                    that.currentState.handleDot(dot.value);
+                    that.updateDisplay();
+                })
+            })
+
+
         }
     }
 
 
-    /* define a set of states*/
-    const FO = {}
+    window.addEventListener("load", (event) => {
+        const calculator = new Calculator();
+        calculator.getInput()
+    }, false);
 
 
 })();
